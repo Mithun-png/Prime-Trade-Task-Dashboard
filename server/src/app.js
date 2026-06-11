c0115e48de6b2677ff79ae60cce7cc7aeaa093b3
Mithun S <smithun.pillai@gmail.com>
2026-06-11 11:43:43 +0530

    Fix updates Cors for production

--- server/src/app.js
+++ server/src/app.js
@@ -7,7 +7,10 @@ const taskRoutes = require("./routes/task.routes");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "https://prime-trade-task-dashboard.vercel.app",
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
